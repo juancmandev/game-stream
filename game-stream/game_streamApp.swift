71021cd2274104e4805347e25ac7a0257cb19904
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-09-29 21:27:05 -0500

    Initial Commit

---
+++ game-stream/game_streamApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_streamApp.swift
+//  game-stream
+//
+//  Created by Juan Carlos Manzanero Domínguez on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct game_streamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
