09d036c4ea5a53c599f239ce82e2d4e488dcc837
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-11-05 10:56:43 -0600

    create login view

--- game-stream/ContentView.swift
+++ game-stream/ContentView.swift
@@ -10,25 +10,89 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            Spacer()
+            
             Color(red: 19/255, green: 30/255, blue: 53/255, opacity: 1.0).ignoresSafeArea()
             
             VStack {
-                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
                 
-                StartAndRegisterView()
+                StartView()
             }
         }
     }
 }
 
-struct StartAndRegisterView: View {
+struct StartView: View {
+    @State var loginType = true
+    
     var body: some View {
         VStack {
             HStack {
-                Text("Login")
-                Text("Sign in")
+                Button("LOGIN") {
+                    loginType = true
+                }.foregroundColor(loginType ? .white : .gray)
+                
+                Spacer()
+                
+                Button("SIGN IN") {
+                    loginType = false
+                }.foregroundColor(loginType ? .gray : .white)
+            }.padding(.horizontal, 32)
+            
+            Spacer(minLength: 42)
+            
+            if loginType {
+                LoginView()
+            } else {
+                SignInView()
             }
         }
+    }
+}
+
+struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Email").foregroundColor(Color("DarkCian"))
+                
+                ZStack(alignment: .leading) {
+                    if email.isEmpty {
+                        Text(verbatim: "example@gmai.com").font(.caption).foregroundColor(.gray)
+                    }
+                    
+                    TextField("", text: $email).foregroundColor(.white).autocapitalization(.none)
+                }
+                
+                Divider().frame(height: 1).background(Color("DarkCian"))
+            }.padding(.horizontal, 32)
+            
+            Divider().frame(height: 28)
+            
+            VStack(alignment: .leading) {
+                Text("Password").foregroundColor(Color("DarkCian"))
+                
+                ZStack(alignment: .leading) {
+                    if password.isEmpty {
+                        Text(verbatim: "Type your password").font(.caption).foregroundColor(.gray)
+                    }
+                    
+                    SecureField("", text: $password).foregroundColor(.white)
+                }
+                
+                Divider().frame(height: 1).background(Color("DarkCian"))
+            }.padding(.horizontal, 32)
+        }
+    }
+}
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign in View").foregroundColor(.white)
     }
 }
 
